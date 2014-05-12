580f0ffc53b4a34c1db2f37a52d89e4674ec6c81
Pierre Langlois <pierre.langlois@embecosm.com>
2014-05-12 16:43:38 +0100

    Add AVR support.
    We are using environment variables to set the triple.
    
    	* dejagnu/baseboards/avr-gdbserver.exp: New board file for running
    	with a gdbserver.
    	* dejagnu/baseboards/avr-sim.exp: New default board file for
    	running with a simulator.
    	* dejagnu/gdb-extra: New file. This fixes a issue with loading a
    	program with dejagnu
    	* dejagnu/gdb-stub-extra: New file. Likewise but also allow GDB
    	to check the return code.
    	* run-avr.sh: New file. Run BEEBS for AVR.
    	* run.sh: Run BEEBS natively on x86.
    	* site.exp: Add AVR to the list of targets.

--- run-avr.sh
+++ run-avr.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# BEEBS Top Level Run script
+# BEEBS Top Level Run script for AVR
 # Copyright (C) 2014 Embecosm Limited
 
 # Contributor Simon Cook <simon.cook@embecosm.com>
@@ -21,10 +21,41 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# The arguments have the following meaning.
+
+# Invocation Syntax
+
+#     run.sh [--target-board <board>]
+
+# --target-board <board>
+
 CURDIR=$(dirname $0)
 export DEJAGNU=$CURDIR/site.exp
 
-export TARGET_TRIPLET=x86_64-unknown-linux-gnu
-export TARGET_ALIAS=x86
+export TARGET_TRIPLET=avr-unknown-none
+export TARGET_ALIAS=avr
 
-runtest --tool=gcc --directory=gcc.beebs --srcdir=$CURDIR execute.exp=gcc.beebs/*.c
+export AVR_MCU="atmega128"
+export AVR_CFLAGS_EXTRA=""
+export AVR_HEAP_END="0x800fff"
+export AVR_LDFLAGS_EXTRA=""
+export AVR_LDSCRIPT=""
+export AVR_NETPORT="51000"
+export AVR_STACK_SIZE="2048"
+export AVR_TEXT_SIZE="131072"
+
+TARGET_BOARD=avr-gdbserver
+
+opt=$1
+case ${opt} in
+  --target-board)
+    shift
+    TARGET_BOARD=$1
+    ;;
+  *)
+    ;;
+esac
+
+runtest --target_board=${TARGET_BOARD} \
+  --tool=gcc --directory=gcc.beebs \
+  --srcdir=$CURDIR execute.exp=gcc.beebs/*.c
