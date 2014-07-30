c45ecf3a08ea66d02166b54c3919f988bcbeb6fb
George Field <gf12815@my.bristol.ac.uk>
2014-07-30 13:02:15 +0100

    Small script to create hadamard matrix.

---
+++ plb/genmat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Generates a matrix1 file
+import sys, math
+from pyDOE import pbdesign;
+
+if (len(sys.argv) < 2):
+    print("USAGE: genmat.py <factor count>")
+    sys.exit()
+
+# Get the number of factors.
+n = int(sys.argv[1])
+
+# Get the nearest N
+N = math.ceil(int(n) / 4) * 4
+
+# Generate design
+mat = pbdesign(N)
+
+# Format the matrix
+for idx, row in enumerate(mat):
+    print("{} {}".format(idx, (' {: 1.0f} '*n).format(*row)))
