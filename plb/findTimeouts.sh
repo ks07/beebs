941d99ee881ca42a2fa06c100e3cf22f5102f38d
George Field <gf12815@my.bristol.ac.uk>
2014-08-18 02:06:16 +0100

    Re-run failed tests using platformrun.

--- plb/findTimeouts.sh
+++ plb/findTimeouts.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 RDIR=$(pwd -P)
-NRGD=$(find . -name 'energy.csv*' -size +0 -printf '%h\n' | grep './run..*')
-TO_FND=0
+NRGD=$(find . -maxdepth 2 -name 'energy.csv*' -size +0 -printf '%h\n' | grep './run-..*' | sort | uniq)
 
 for d in $NRGD; do
     cd "$d" &&
     # Looks for tests in an energy.csv that have timed out, and attempts to re-run them.
     TMOUT=$(grep -x '.*,0,0,0,0,0' energy.csv* | sed 's/,0,0,0,0,0//')
 
-    if [ -n "$TMOUT" ]; then
-	(( TO_FND++ ))
-	echo "TIMEOUTS IN: $d"
-    fi
+    # Make some backups before we start modifying files
+    find . -maxdepth 1 -name 'energy.csv*' -size +0 -exec cp {} {}.bkp \;
+
+    for tst in $TMOUT; do
+	NFILE=$(echo "$tst" | cut -d ':' -f 1)
+	BMARK=$(echo "$tst" | cut -d ':' -f 2)
+
+	if [ -f "src/$BMARK/$BMARK" ]; then
+	    echo "Re-running $BMARK in $NFILE in $d"
+	    OUT=$(platformrun --csv atmega328p "src/$BMARK/$BMARK" | sed 's/, /,/g')
+	    OUT="${BMARK},${OUT}"
+	    echo "Got: ${OUT}"
+	    sed -i "s/^${BMARK},0,0,0,0,0$/${OUT}/" "$NFILE"
+	    
+	else
+	    echo "Can't re-run $BMARK"
+	fi
+    done
 
     cd "$RDIR"
 done
-
-if [[ "$TO_FND" -eq 0 ]]; then
-    echo "NO TIMEOUTS FOUND"
-fi
 
 exit
